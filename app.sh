180c487e3a45f6979f11dc80686d1fdd3d85e62e
slinjing <82428737+slinjing@users.noreply.github.com>
2024-10-18 11:26:10 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -26,16 +26,18 @@ while IFS= read -r image; do
     echo "正在处理镜像: ${image_name}:$image_tag"
     echo "拉取镜像: ${image_name}:$image_tag"
     docker pull $image
-    docker images 
-    # if [ $? -eq 0 ]; then
-    #     echo "镜像: $image 拉取完成"
-    #     # 获取镜ID:
-    #     # last_image=$(docker images --format '{{.Repository}}:{{.Tag}}' -q | tail -1)
-    #     # image_id=$(docker images | grep $image_name | awk '{print $3}')
-    #     image_id=$(docker images --format '{{.ID}}' | tail -n 1)
-    #     a=$(docker images --format '{{.Repository}}:{{.Tag}}' | tail -n 1)
-    #     echo "镜像ID: $image_id"
-    #     echo "镜像名称: $a"
+    
+    if [ $? -eq 0 ]; then
+        echo "镜像: $image 拉取完成"
+        # 获取镜ID:
+        # last_image=$(docker images --format '{{.Repository}}:{{.Tag}}' -q | tail -1)
+        # image_id=$(docker images | grep $image_name | awk '{print $3}')
+        image_id=$(docker images --format '{{.ID}}' | tail -n 1)
+        a=$(docker images --format '{{.Repository}}:{{.Tag}}' | tail -n 1)
+        echo "镜像ID: $image_id"
+        echo "镜像名称: $a"
+
+        docker images 
 
         
     #     docker tag $image_id $target_image
